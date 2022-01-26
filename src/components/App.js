bced59732d7aad5e77958b37c5c90c23e4354448
Stanislav <infleshlightwetrust@gmail.com>
2022-01-26 11:03:37 +0300

    initialize

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "./App.less";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Dynamic Content Project</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "build"),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            ['@babel/preset-react', { "runtime": "automatic" }]
+                        ]
+                    }
+                },
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif|ico)$/,
+                type: 'asset/resource',
+            },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+            // favicon: path.join(__dirname, "src", "favicon.ico"),
+        }),
+    ],
+}
