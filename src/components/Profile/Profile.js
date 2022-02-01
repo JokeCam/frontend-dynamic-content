60846dda9edc658211f4a19865ebef4fba713f8e
Stanislav <infleshlightwetrust@gmail.com>
2022-02-01 13:56:15 +0300

    create-profile-component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,40 @@
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
+
+import Post from "./Post/Post";
+
+import "./Profile.less";
+
+function Profile() {
+    const userContext = useContext(CurrentUserContext);
+
+    console.log(userContext);
+
+    function handlePostsDisplay() {
+        console.log(userContext)
+        return userContext.posts?.map((elem) => {
+            return <Post data={elem} key={elem.id} />
+        });
+    };
+
+    return (
+        <div className="profile">
+            <div className="profile__main-container">
+                <div className="profile__avatar-container">
+                    <img className="profile__avatar" src={userContext.avatar}/>
+                </div>
+                <div className="profile__info-container">
+                    <h2 className="profile__name">{userContext.name}</h2>
+                    <p className="profile__about">{userContext.about}</p>
+                </div>
+            </div>
+            <div className="profile__posts">
+                {
+                    handlePostsDisplay()
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
