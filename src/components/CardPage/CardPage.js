314d0a2af68d2841be157f276bd91eed74403f42
Stanislav <infleshlightwetrust@gmail.com>
2022-02-03 11:10:30 +0300

    bugfixes/not-using-regex-in-search

--- src/components/CardPage/CardPage.js
+++ src/components/CardPage/CardPage.js
@@ -7,6 +7,7 @@ import "./CardPage.less";
 
 function CardPage() {
     const [ cards, setCards ] = useState([]);
+    const [ displayedCards, setDisplayedCards ] = useState([]);
     const [ searchInputValue, setSearchInputValue ] = useState("");
 
     useEffect(() => {
@@ -18,31 +19,32 @@ function CardPage() {
             getCards();
         };
 
-        const tagRegex = new RegExp(searchInputValue.slice(1), "ig");
-        const descriptionRegex = new RegExp(searchInputValue, "ig");
-
         const filteredCards = cards.filter((elem) => {
             if(searchInputValue[0] === "#") {
                 const tagsArr = elem.tags.filter((tag) => {
-                    return tag.search(tagRegex) > -1;
-                })
+
+                    return tag.includes(searchInputValue.slice(1));
+                });
+
                 return tagsArr.length > 0;
             } else {
-                return elem.description.search(descriptionRegex) > -1;
+
+                return elem.description.includes(searchInputValue);
             };
         });
 
-        setCards(filteredCards);
+        setDisplayedCards(filteredCards);
 
     }, [searchInputValue]);
 
     async function getCards() {
         const fetchedCards = await service.fetchCards();
+        setDisplayedCards(fetchedCards);
         setCards(fetchedCards);
     };
 
     function handleCardsDisplay() {
-        return cards.map((elem) => {
+        return displayedCards.map((elem) => {
             return <Card data={elem} key={elem.id}/>
         });
     };
