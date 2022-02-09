afb7d2f81c2d3265660a93c23cc6f9e4a6847a57
Stanislav <infleshlightwetrust@gmail.com>
2022-02-09 11:21:19 +0300

    bugfix/review-fixes

--- src/components/Profile/Post/Post.js
+++ src/components/Profile/Post/Post.js
@@ -1,46 +1,19 @@
-import classnames from "classnames";
-import { useState } from "react";
-
 import "./Post.less";
 
 function Post(props) {
-    const [ isMouseOverPost, setIsMouseOverPost ] = useState(false);
-
-    const postImageClass = classnames(
-        "post__image",
-        {
-            "post__image_murk": isMouseOverPost
-        }
-    );
-
-    const postOverlayClass = classnames(
-        "post__overlay",
-        {
-            "post__overlay_displayed": isMouseOverPost
-        }
-    );
-
-    function handleMouseOver() {
-        setIsMouseOverPost(true);
-    };
-
-    function handleMouseLeave() {
-        setIsMouseOverPost(false);
-    };
-    
     return (
-        <div className="post" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
-            <div className={postOverlayClass}>
+        <div className="post">
+            <div className="post__overlay">
                 <div className="post__overlay-container">
                     <div className="post__overlay-like-icon"/>
-                    <p className="post__overlay-like-span">{props.data.likes.length}</p>
+                    <p className="post__overlay-like-span">{props.likes}</p>
                 </div>
                 <div className="post__overlay-container">
                     <div className="post__overlay-comment-icon"/>
-                    <p className="post__overlay-comment-span">{props.data.comments.length}</p>
+                    <p className="post__overlay-comment-span">{props.comments}</p>
                 </div>
             </div>
-            <img className={postImageClass} src={props.data.image}/>
+            <img className="post__image" src={props.image}/>
         </div>
     );
 };
